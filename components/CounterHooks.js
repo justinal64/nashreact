dcce85a58ac4017f78c1b2e356ab77504790cc07
justin leggett <justin@codingrookie.com>
2019-01-26 00:14:13 -0600

    added useEffect example and added an example of a custom hook

--- components/CounterHooks.js
+++ components/CounterHooks.js
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import { styles } from "../styles/styles";
+import { useFetchJsonData } from "./useFetchJsonData";
+
 export default function CounterHooks() {
-  const [count, setCount] = useState(0); // useState returns and array and [count, setCount] is being destructured
+  const [count, setCount] = useState(0); // useState returns an array [count, setCount] and is being destructured
+  const result = useFetchJsonData();
+  console.log("result: ", result.result);
+  // let [name, priorities, location, school, work] = useFetchJsonData();
   const increment = () => setCount(count + 1);
   const reset = () => setCount(0);
   return (
@@ -13,6 +18,7 @@ export default function CounterHooks() {
         Reset
       </button>
       <p>Count: {count}</p>
+      {/* <p>{name}</p> */}
     </div>
   );
 }
